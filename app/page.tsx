a264af526eaf0514ef5b310a2dd8b520ca59243e
okivictor01-bit <okivictor01@gmail.com>
2026-08-21 14:59:52 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Godsgift Store</h1>
+      <p>Your amazing products will be listed here soon!</p>
+    </main>
+  )
+}
