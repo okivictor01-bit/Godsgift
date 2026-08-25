39332bc178b71906ab8f73d523ad7c017a0f7ab6
okivictor01-bit <okivictor01@gmail.com>
2026-08-25 01:02:55 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useState } from 'react';
-import Footer from '@/components/Footer';
 
 export default function Contact() {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
