6beedc6378da65a3c114fe32a07c4a6ec05c5b6f
okivictor01-bit <okivictor01@gmail.com>
2026-08-26 14:35:54 +0100

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
    import { supabase } from '../../../lib/supabaseClient';
-import { supabase } from '../../lib/supabaseClient';
 
 interface Product {
   id: string;
